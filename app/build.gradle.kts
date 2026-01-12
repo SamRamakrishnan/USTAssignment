df05e86cdddaa8214ed74bcf66c67807b4d400ef
Shyam Ramakrishnan <shyamsundar1010@gmail.com>
2026-01-12 14:09:21 +0530

    Gradle version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.testproject.ustassignment"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.testproject.ustassignment"
